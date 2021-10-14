ed287ac19f2f14d14b9f94c95555a406c8a3fad3
Arisa Miyamoto <ari_1019_jump@icloud.com>
2021-10-14 12:14:48 +0900

    update

--- src/Ch01.hs
+++ src/Ch01.hs
@@ -1,16 +1,41 @@
 module Ch01 where
 
 -- 00. "stressed" の逆順
+{- |
+>>> ans00
+"desserts"
+-}
 ans00 :: String
 ans00 = reverse "stressed"
-
+-- reverse :: [a] -> [a] はリストを逆転にする
+{- |
+>>> putStrLn omake00
+ぞたけやぶやけた
+-}
 omake00 :: String
 omake00 = reverse "たけやぶやけたぞ"
 
 -- 01. パタトカクシーー
 ans01 :: String
-ans01 = take0dds "パタトクカシーー"
+ans01 = takeOdds "パタトクカシーー"
 
-take0dds :: String -> String
-take0dds [] = ""
-take0dds (c:cs) = c : take0dds (drop 1 cs)
+{- | 文字列から奇数番目の文字だけをとりだす
+>>> putStrLn (takeOdds "パタトクカシーー")
+パトカー
+>>>putStrLn (takeOdds "hogehoge")
+hghg
+-}
+takeOdds :: [a] -> [a]
+takeOdds []     = [] 
+takeOdds (c:cs) = c : takeEvens cs
+
+{- | リストから偶数番目の要素だけとりだす
+>>>putStrLn (takeEvens "パタトクカシーー")
+タクシー
+-}
+takeEvens :: [a] -> [a]
+takeEvens [] = []
+takeEvens (c:cs) = takeOdds cs
+
+sample :: String
+sample = "パタトクカシーー"
